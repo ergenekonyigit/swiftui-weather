3a5e0f3333941c52bb2e77528a11e03399ba649c
ergenekonyigit <ergenekon.yigit@trendyol.com>
2021-01-30 19:56:05 +0300

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Ergenekon Yiğit on 30.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
